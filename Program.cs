aaa7161ac74630ce70811a40eccf48c39409dbbd
ste ste <steelst0@gmail.com>
2018-07-28 19:32:56 -0700

    3 of 13

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace Basic_13
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Print 1-255:");
+            //// Print all the numbers from 1 to 255.
+            // Console.WriteLine("Print 1-255:");
+            // for(int i=1; i<=255; i++)
+            // {
+                // Console.WriteLine(i);
+            // }
+
+            ////Print all the odd numbers from 1 to 255.
+            // Console.WriteLine("Print odd numbers between 1-255:");
+            // for(int i=1; i<=255; i++)
+            // {
+                // if(i%2==1)
+                // {
+                    // Console.WriteLine(i);
+                // }
+            // }
+
+            ////Print the numbers from 0 to 255, but this time, 
+            ////also print the sum of the numbers that have been printed so far.
+            Console.WriteLine("Print Sum:");
+            int sum = 0;
+            for(int i=1; i<=255; i++)
+            {
+                sum+=i;
+                Console.WriteLine("New Number: {0} Sum: {1}", i, sum);
+            }
         }
     }
 }
